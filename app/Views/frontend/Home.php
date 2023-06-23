344de4be811844c9e194b13996bb2d9369a674d1
angindel <angindel@gmail.com>
2023-06-23 17:32:24 +0700

    home and produk edit

--- app/Views/frontend/Home.php
+++ app/Views/frontend/Home.php
@@ -6,6 +6,20 @@
     <link rel="stylesheet" type="text/css" href="<?= base_url('asset/css/style-local.css') ?>">
     <link rel="stylesheet" href="<?= base_url("owlcarousel/dist/assets/owl.carousel.min.css") ?>">
 <link rel="stylesheet" href="<?= base_url("owlcarousel/dist/assets/owl.theme.default.min.css") ?>">
+<style type="text/css">
+ .page-load-status {
+  display: none; /* hidden by default */
+  padding-top: 20px;
+  border-top: 1px solid #DDD;
+  text-align: center;
+  color: #777;
+}
+#head-slide  .item img{
+    display: block;
+    width: 100%;
+    height: auto;
+}
+</style>
 <?= $this->endSection() ?>
 
 <?= $this->section('cdn-foot') ?>
@@ -36,84 +50,104 @@
 </nav>
 
 <main class="container-fluid">
-  <!-- Carousel -->
-  <div class="row my-0 py-0">
-  <div id="head-slide" class="owl-carousel owl-theme">
-    <div class="item">
-      <img src="<?= base_url() ?>asset/foto_slide/hijab1.jpg" alt="Los Angeles" class="w-100">
-    </div>
-    <div class="item">
-      <img src="<?= base_url() ?>asset/foto_slide/hijab2.jpg" alt="Chicago" class="w-100">
-    </div>
-    <div class="item">
-      <img src="<?= base_url() ?>asset/foto_slide/hijab3.jpg" alt="New York" class="w-100">
-    </div>
-  </div>
-</div>
-  <!-- END Carousel -->
-<div class="row pb-1">
-    <?php foreach($kategori as $row) { ?>
-    <div class="col-2">
-        <div class="card">
-            <div class="card-body">
-                <a href="<?= $row->nama_kategori ?>"></a>
-            </div>
+    <!-- SLIDE -->
+    <div class="row">
+      <div id="head-slide" class="owl-carousel owl-theme p-0">
+        <div class="item">
+          <img src="<?= base_url() ?>asset/foto_slide/hijab1.jpg" alt="Los Angeles">
         </div>
+        <div class="item">
+          <img src="<?= base_url() ?>asset/foto_slide/hijab2.jpg" alt="Chicago">
+        </div>
+        <div class="item">
+          <img src="<?= base_url() ?>asset/foto_slide/hijab3.jpg" alt="New York">
+        </div>
+      </div>
     </div>
-    <?php } ?>
-</div>
-    <div id="produk-slide" class="owl-carousel owl-theme">
-        <?php foreach($produk_new as $row) { ?>
-        <div class="card p-1 mr-1">
-            <a href="<?= base_url("produk/detail/$row->produk_seo") ?>">
-                <img src="<?= base_url() ?>uploads/produk/<?= $row->gambar ?>" class="card-img-top" alt="...">
-            </a>
-            <div class="card-body p-0 m-1">
-                <p class="card-title" style="font-size:12px;">Rp.<?= rupiah($row->harga_konsumen) ?></p>
-                <a href="<?= base_url("produk/detail/$row->produk_seo") ?>" style="text-decoration: none;"><h5 class="card-text lh-sm caption-pb "><?= $row->nama_produk ?></h5></a>
+    <!-- END SLIDE -->
+    <!-- KATEGORI -->
+    <div class="row pb-1 my-3">
+        <?php foreach($kategori as $row) { ?>
+        <div class="col-2">
+            <div class="card">
+                <div class="card-body">
+                    <a href="<?= $row->nama_kategori ?>"></a>
+                </div>
             </div>
         </div>
         <?php } ?>
     </div>
-
-            <div class="row bg-primary align-items-center ps-2 py-2 my-2">
-                <div class="col-8 m-0 p-0 justify-content-start">
-                    <h2 class="align-items-stretch m-0 p-0">EBYKARYA TERBARU</h2>
-                </div>
-                <div class="col-4 text-end m-0 p-0">
-                    <a href="#" class="btn btn-primary btn-sm btn-lc me-2" title="Produk Terbaru" rel="category">SEMUA</a>
+    <!-- END KATEGORI -->
+    <!-- PRODUK TERLARIS -->
+    <div class="row"><h2 class="fw-bold text-center">PRODUK TERLARIS</h2></div>
+    <div class="row">
+        <div id="produk-slide" class="owl-carousel owl-theme">
+            <?php foreach($produk_new as $row) { ?>
+            <div class="card p-1 mr-1">
+                <a href="<?= base_url("produk/detail/$row->produk_seo") ?>">
+                    <img src="<?= base_url() ?>uploads/produk/<?= $row->gambar ?>" class="card-img-top" alt="...">
+                </a>
+                <div class="card-body p-0 m-1">
+                    <p class="card-title" style="font-size:12px;">Rp.<?= rupiah($row->harga_konsumen) ?></p>
+                    <a href="<?= base_url("produk/detail/$row->produk_seo") ?>" style="text-decoration: none;"><h5 class="card-text lh-sm caption-pb "><?= $row->nama_produk ?></h5></a>
                 </div>
             </div>
-            <div id="main-infscroll">
-            <div id="post-infscroll" class="row row-cols-2 row-cols-md-4 row-cols-lg-6 p-0 m-0">
-                <?php foreach($produk_new as $row) { ?>
-                  <div class="col px-1 mb-1">
-                    <div class="card h-100">
-                        <a href="<?= base_url("produk/detail/$row->produk_seo") ?>">
-                            <img src="<?= base_url() ?>uploads/produk/<?= $row->gambar ?>" class="card-img-top" alt="...">
-                        </a>
-                        <div class="card-body p-0 m-1">
-                            <h5 class="card-title">Rp.<?= rupiah($row->harga_konsumen) ?></h5>
-                            <a href="<?= base_url("produk/detail/$row->produk_seo") ?>" style="text-decoration: none;"><h3 class="card-text lh-sm caption-pb "><?= $row->nama_produk ?></h3></a>
-                        </div>
-                        <ul class="list-group list-group-flush text-end border border-top-0">
-                            <li class="list-group-item p-0 m-1"><a href="<?= base_url("produk/detail/$row->produk_seo") ?>" class="btn btn-primary btn-sm">Lihat Detil</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <?php } ?>
-            </div>
-            <a class="pagination__next btn btn-sm">Next</a>
+            <?php } ?>
         </div>
+    </div>
+    <!-- END PRODUK TERLARIS -->
+    <!-- KONTEN -->
+    <section id="konten" class="border-bottom pb-3 border-dark-subtle">
+        <div class="row bg-primary align-items-center ps-2 py-2 my-2">
+            <div class="col-8 m-0 p-0 justify-content-start">
+                <h2 class="align-items-stretch m-0 p-0">EBYKARYA TERBARU</h2>
+            </div>
+            <div class="col-4 text-end m-0 p-0">
+                <a href="#" class="btn btn-primary btn-sm btn-lc me-2" title="Produk Terbaru" rel="category">SEMUA</a>
+            </div>
+        </div>
+        <div id="post-infscroll">
+        </div>
+        <div class="page-load-status">
+            <div class="loader-ellips infinite-scroll-request">
+                <span class="loader-ellips__dot"></span>
+                <span class="loader-ellips__dot"></span>
+                <span class="loader-ellips__dot"></span>
+                <span class="loader-ellips__dot"></span>
+            </div>
+            <p class="infinite-scroll-last">Akhir Konten</p>
+            <p class="infinite-scroll-error font-monospace fs-4 fw-bold">Tidak ada lagi halaman untuk dimuat</p>
+        </div>
+        <div class="row">
+            <div class="col text-center">
+                <button class="btn btn-lg btn-primary view-more-button">Lihat Lebih Banyak</button>
+            </div>
+        </div>
+    </section>
+    <!-- END KONTEN -->
 
-
-  <div class="row pt-5 mt-5">
-      <div class="col">PAYMENT CHANNEL</div>
-      <div class="col">DELIVERY SERVICE</div>
-  </div>
-  <div class="row">
-      <div class="col">FOOTER</div>
-  </div>
+    <div class="row">
+      <div class="col-md-12 col-lg-6 border border-top-0 border-dark-subtle my-2">
+          <div class="row">
+              <div class="col-12">PAYMENT CHANNEL</div>
+              <div class="col-2">BCA</div>
+              <div class="col-2">MANDIRI</div>
+              <div class="col-2">BRI</div>
+              <div class="col-2">BNI</div>
+              <div class="col-2">OVO</div>
+              <div class="col-2">GOPAY</div>
+              <div class="col-2">DANA</div>
+          </div>
+      </div>
+      <div class="col-md-12 col-lg-6 border border-top-0 border-dark-subtle my-2">
+          <div class="row">
+              <div class="col-12">Delivery Service</div>
+              <div class="col-2">JNE</div>
+              <div class="col-2">J&T</div>
+              <div class="col-2">TIKI</div>
+          </div>
+      </div>
+    </div>
 </main>
 <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
 <script src="<?= base_url("owlcarousel/dist/owl.carousel.min.js") ?>"></script>
@@ -156,16 +190,70 @@
           }
         });
 
-        $(".pagination__next").on("click", function(){
-          $.ajax({
-            url: "<?= base_url('tes/2') ?>",
-            type : 'get',
-            success: function(data){
-              $("#post-infscroll>.col:last").after(data).show().fadeIn("slow");
-            },
-          });
+        // $(".pagination__next").on("click", function(){
+        //   $.ajax({
+        //     url: "<?= base_url('tes/2') ?>",
+        //     type : 'get',
+        //     success: function(data){
+        //       $("#post-infscroll>.col:last").after(data).show().fadeIn("slow");
+        //     },
+        //   });
+        // });
+
+        let $maininfscroll = $('#post-infscroll').infiniteScroll({
+              // options
+              path: function(){
+                console.log("page index :"+this.pageIndex);
+                console.log("load count :"+this.loadCount);
+                return `tes/${this.loadCount}`;
+              },
+              append: false,
+              history: false,
+              button: '.view-more-button',
+              scrollThreshold: false,
+              elementScroll : 'div.col',
+              status: '.page-load-status',
+              responseBody: 'json',
+            });
+
+        $maininfscroll.on('load.infiniteScroll', function(event, body){
+            // compile body data into HTML
+          let itemsHTML = body.map( getItemHTML ).join('');
+          itemsHTML = '<div class="row row-cols-3 row-cols-md-5 p-0 m-0">' + itemsHTML + '</div>';
+          console.log(itemsHTML);
+          // convert HTML string into elements
+          let $items =  $( itemsHTML );
+          console.log($items);
+          // append item elements
+          $maininfscroll.infiniteScroll( 'appendItems', $items );
         });
+
+// load initial page
+$maininfscroll.infiniteScroll('loadNextPage');
+
+//------------------//
+
+function getItemHTML({base, nama_produk, harga_konsumen, gambar, produk_seo}) {
+    return `<div class="col px-1 mb-1">
+                    <div class="card h-100">
+                        <a href="${base}produk/detail/${produk_seo} ">
+                            <img src="${base}uploads/produk/${gambar}" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body p-0 m-1">
+                            <h5 class="card-title">Rp.${harga_konsumen}</h5>
+                            <a href="${base}produk/detail/${produk_seo}" style="text-decoration: none;"><h3 class="card-text lh-sm caption-pb">${nama_produk}</h3></a>
+                        </div>
+                        <ul class="list-group list-group-flush text-end border border-top-0">
+                            <li class="list-group-item p-0 m-1"><a href="${base}produk/detail/${produk_seo}" class="btn btn-primary btn-sm">Lihat Detil</a></li>
+                        </ul>
+                    </div>
+                </div>`;
+}
+
     });
+
+    
+
 </script>
 
 <?= $this->endSection() ?>
--- app/Views/frontend/Produk_detail.php
+++ app/Views/frontend/Produk_detail.php
@@ -3,30 +3,36 @@
 <?= $this->section('cdn-head') ?>
     <?= $this->include('assets/local/bs_css-5.3.0') ?>
     <link rel="stylesheet" type="text/css" href="<?= base_url('asset/css/style-local.css') ?>">
-<?= $this->endSection('cdn-head') ?>
+    <style type="text/css">
+        #dgambar  div  img {
+            width: 100%;
+            height: 100%;
+        }
+        .nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active {
+  color: var(--bs-nav-tabs-link-active-color);
+  background-color: var(--bs-success-border-subtle);
+}
+    </style>
+<?= $this->endSection() ?>
 
 <?= $this->section('cdn-foot') ?>
     <?= $this->include('assets/local/bs_js-5.3.0') ?>
-<?= $this->endSection('cdn-head') ?>
+<?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
 <body>
 <nav class="navbar sticky-top navbar-expand-md bg-light" data-bs-theme="light">
     <?= $this->include('layout/home/navbar') ?>
 </nav>
-<main class="container">
+<main class="container-fluid px-5">
         <div id="product-detail" class="card my-5 border border-0">
-            <div class="row g-0">
-                <div class="col-sm-12 col-md-4">
-                    <a href="">
-                        <img src="<?= base_url() ?>uploads/produk/<?= $produk->gambar ?>" class="img-fluid rounded-start w-75 h-auto mx-auto d-block" alt="...">
-                    </a>
+            <div id="dgambar" class="row g-0">
+                <div class="col-sm-12 col-md-4 p-0 m-0">
+                    <img src="<?= base_url() ?>uploads/produk/<?= $produk->gambar ?>" class="rounded-start mx-auto d-block" alt="...">
                 </div>
                 <div class="col-sm-12 col-md-8 font-monospace">
-                    <div class="card-header p-0 mx-2 border border-0">
-                        <h5 class="pd-h"><?= $produk->nama_produk ?></h5>
-                    </div>
                     <div class="card-body m-0">
+                        <h5 class="pd-h"><?= $produk->nama_produk ?></h5>
                         <p class="rp card-text m-0 font-monospace">Rp <?= $produk->harga_konsumen ?></p>
                         <div class="row px-0 mx-0">
                             <div class="card-text row px-0 border-bottom">
@@ -39,7 +45,30 @@
                             </div>
                             <div class="card-text row px-0 border-bottom">
                                 <small class="m-0 col-4 fw-bold text-secondary-emphasis">Warna</small>
-                                <small class="m-0 col-8 fw-bold text-danger ">Hitam | Merah | Biru</small>
+                                <small class="col-md-8 col-lg-8 fw-bold text-danger m-0">
+                                    <div id="pwarna" class="d-flex flex-wrap">
+                                        <div class="p-1 m-1 rounded-1 border border-2 border-secondary ">Hitam</div>
+                                        <div class="p-1 m-1 rounded-1 border border-2 border-secondary">Merah</div>
+                                        <div class="p-1 m-1 rounded-1 border border-2 border-secondary">Biru</div>
+                                        <div class="p-1 m-1 rounded-1 border border-2 border-secondary ">Kuning</div>
+                                        <div class="p-1 m-1 rounded-1 border border-2 border-secondary">Hijau</div>
+                                        <div class="p-1 m-1 rounded-1 border border-2 border-secondary">Merah Muda</div>
+                                        <div class="p-1 m-1 rounded-1 border border-2 border-secondary">Abu - Abu</div>
+                                    </div>
+                                </small>
+                            </div>
+                            <div class="card-text row px-0 border-bottom">
+                                <small class="m-0 col-4 fw-bold text-secondary-emphasis">Size</small>
+                                <small class="col-md-8 col-lg-8 fw-bold text-danger m-0">
+                                    <div id="psize" class="d-flex flex-wrap">
+                                        <div class="py-1 px-3 m-1 rounded-1 border border-2 border-secondary ">S</div>
+                                        <div class="py-1 px-3 m-1 rounded-1 border border-2 border-secondary">M</div>
+                                        <div class="py-1 px-3 m-1 rounded-1 border border-2 border-secondary">L</div>
+                                    </div>
+                                </small>
+                            </div>
+                            <div class="card-text row px-0">
+                                <button class="btn btn-lg btn-success"><svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg> Beli via WhatsApp</button>
                             </div>
                         </div>
                         
@@ -47,23 +76,18 @@
                 </div>
             </div>
         </div>
-    <div class="row">
-            <div class="card-header">
-                <h5 class="pd-h mt-5">Deskripsi Produk</h5>
-            </div>
-        <div class="col-sm-12 col-md-8">
-            <div class="card-body m-0" style="font-family: Roboto;">
-                <p class="card-text">
-                    <?= $produk->keterangan ?>
-                </p>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="card-header">
-            <h5 class="pd-h mt-5">Penilaian Produk</h5>
-        </div>
-        <div class="col-sm-12 col-md-8">
+<div class="row">
+        <nav>
+  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+    <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-detail" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Deskripsi Produk</button>
+    <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-penilaian" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Penilaian</button>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+  <div class="tab-pane fade show active" id="nav-detail" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0"><?= $produk->keterangan ?></div>
+  <div class="tab-pane fade" id="nav-penilaian" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+      <div class="row">
+          <div class="col-sm-12 col-md-8">
             <div class="card-body m-0">
                 <p class="card-text">
                     SEMUA | 5 BINTANG | 4 BINTANG | 3 BINTANG | 2 BINTANG | 1 BINTANG | 
@@ -77,7 +101,11 @@
                 <p class="card-text">| KOMENTAR |</p>
             </div>
         </div>
-    </div>
+      </div>
+  </div>
+</div>
+</div>
+
     <div class="row">
         <div class="col">
             <div>PRODUK TERKAIT</div>
@@ -93,5 +121,28 @@
         </div>
     </div>
 </main>
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<script type="text/javascript">
+    $('#pwarna > div').on('click', function(){
+        $('#pwarna > div').removeClass('border-primary');
+        $('#pwarna > div').removeClass('border-3');
+        $('#pwarna > div').addClass('border-2');
+        $('#pwarna > div').addClass('border-secondary');
+        $(this).removeClass('border-2')
+        $(this).removeClass('border-secondary');
+        $(this).addClass('border-3');
+        $(this).addClass('border-primary');
+    });
+    $('#psize > div').on('click', function(){
+        $('#psize > div').removeClass('border-primary');
+        $('#psize > div').removeClass('border-3');
+        $('#psize > div').addClass('border-2');
+        $('#psize > div').addClass('border-secondary');
+        $(this).removeClass('border-2');
+        $(this).removeClass('border-secondary');
+        $(this).addClass('border-3');
+        $(this).addClass('border-primary');
+    });
+</script>
 </body>
 <?= $this->endSection() ?>
